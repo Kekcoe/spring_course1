32569c0f89a2908d788cfb39f3cecb57efa1f061
Oleg Razin <kekcoe@gmail.com>
2022-03-03 19:27:44 +0300

    @autowired для field of class

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
+    @Autowired
     private Pet pet;
     private String surname;
     private int age;
@@ -14,10 +15,16 @@ public class Person {
 //        System.out.println("Person bean is created");
 //        this.pet = pet;
 //    }
-     @Autowired
+
+ //    @Autowired
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
         this.pet = pet;
+    }
+
+
+    public Person() {
+        System.out.println("Person bean is created") ;
     }
 
     public String getSurname() {
