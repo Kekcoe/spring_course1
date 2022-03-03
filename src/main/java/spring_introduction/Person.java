d2a9a8357019d3b4f5b5f63fa2c9b5e342096ad4
Oleg Razin <kekcoe@gmail.com>
2022-03-03 19:23:45 +0300

    @autowired для сеттера

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -9,12 +9,12 @@ public class Person {
     private String surname;
     private int age;
 
-    @Autowired
-    public Person(Pet pet) {
-        System.out.println("Person bean is created");
-        this.pet = pet;
-    }
-
+//    @Autowired
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+     @Autowired
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
         this.pet = pet;
