291cf5135fd58b43192059c06ce34fb361f1705f
Oleg Razin <kekcoe@gmail.com>
2022-03-04 09:39:03 +0300

    Конфигурация с помощью java кода часть 2

--- src/main/java/spring_introduction/MyConfig.java
+++ src/main/java/spring_introduction/MyConfig.java
@@ -1,10 +1,24 @@
 package spring_introduction;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@ComponentScan("spring_introduction")
+//@ComponentScan("spring_introduction")
 public class MyConfig {
+
+    @Bean
+    @Scope("singleton")
+    public Pet catBean(){
+        return new Cat();
+    }
+
+    @Bean
+    public Person personBean(){
+        return new Person(catBean());
+    }
+
 }
--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -17,23 +17,26 @@ public class Person {
   //  @Value("36")
     private int age;
 
-    @Autowired
-    public Person( @Qualifier("dog")Pet pet) {
-        System.out.println("Person bean is created");
+    public Person(Pet pet) {
+        System.out.println("Person bean is created") ;
         this.pet = pet;
     }
 
 //    @Autowired
+//    public Person( @Qualifier("dog")Pet pet) {
+//        System.out.println("Person bean is created");
+//        this.pet = pet;
+//    }
+
+//    @Autowired
 //    @Qualifier("dog")
-    public void setPet(Pet pet) {
-        System.out.println("Class Person: set Pet");
-        this.pet = pet;
-    }
+//    public void setPet(Pet pet) {
+//        System.out.println("Class Person: set Pet");
+//        this.pet = pet;
+//    }
 
 
-    public Person() {
-        System.out.println("Person bean is created") ;
-    }
+
 
     public String getSurname() {
         return surname;
