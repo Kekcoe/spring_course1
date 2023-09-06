c98a21c373f3102043906b18819447cefc065467
razinoe <kekcoe@gmail.com>
2023-09-06 11:59:46 +0300

    Урок 33. Advice тип @AfterThrowing

--- src/main/java/aop/Test2.java
+++ src/main/java/aop/Test2.java
@@ -8,12 +8,14 @@ public class Test2 {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig1.class);
-        University university = context.getBean("university", University.class);
-
+        University university =  context.getBean("university", University.class);
+try{
         university.addStudents();
         List<Student> students = university.getStudents();
-        System.out.println(students);
-
+        System.out.println(students);}
+catch(Exception e){
+    System.out.println(" Was throwing exception " + e);
+}
         context.close();
     }
 
--- src/main/java/aop/apects/UniversityLoggingAspect.java
+++ src/main/java/aop/apects/UniversityLoggingAspect.java
@@ -2,6 +2,7 @@ package aop.apects;
 
 import aop.Student;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -12,27 +13,31 @@ import java.util.List;
 @Aspect
 public class UniversityLoggingAspect {
 
-    @Before("execution(* getStudents())")
-    public void beforeGetStudentsLoggingAdvice(){
-        System.out.println("beforeGetStudentsLoggingAdvice : we are logging getting of list of students " +
-                "before method getStudents");
+//    @Before("execution(* getStudents())")
+//    public void beforeGetStudentsLoggingAdvice() {
+//        System.out.println("beforeGetStudentsLoggingAdvice : we are logging getting of list of students " +
+//                "before method getStudents");
+//    }
+
+//    @AfterReturning(pointcut = "execution(* getStudents())",
+//    returning = "students")
+//    public void afterReturningGetStudentsLoggingAdvice(List<Student> students){
+//        Student firstStudent = students.get(0);
+//
+//        String nameSurname = firstStudent.getNameSurname();
+//        nameSurname = "Mr " + nameSurname;
+//        firstStudent.setNameSurname(nameSurname);
+//
+//        double avgGrade = firstStudent.getAvgGrade();
+//        avgGrade = avgGrade + 1;
+//        firstStudent.setAvgGrade(avgGrade);
+//
+//        System.out.println("afterReturningGetStudentsLoggingAdvice : we are logging getting of list of students " +
+//                "after method getStudents");
+//    }
+
+    @AfterThrowing(pointcut="execution(* getStudents())", throwing="example")
+    public void afterThrowingGetStudentsLoggingAdvice(Throwable example) {
+        System.out.println("afterThrowingGetStudentsLoggingAdvice: logging of throwing exception " + example);
     }
-
-    @AfterReturning(pointcut = "execution(* getStudents())",
-    returning = "students")
-    public void afterReturningGetStudentsLoggingAdvice(List<Student> students){
-        Student firstStudent = students.get(0);
-
-        String nameSurname = firstStudent.getNameSurname();
-        nameSurname = "Mr " + nameSurname;
-        firstStudent.setNameSurname(nameSurname);
-
-        double avgGrade = firstStudent.getAvgGrade();
-        avgGrade = avgGrade + 1;
-        firstStudent.setAvgGrade(avgGrade);
-
-        System.out.println("afterReturningGetStudentsLoggingAdvice : we are logging getting of list of students " +
-                "after method getStudents");
-    }
-
 }
